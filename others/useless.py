9cae22d8c88d04bd19e51623ed41e8805651aaed
stefanodallapalma <stefano.dallapalma0@gmail.com>
2021-06-01 16:15:13 +0200

    Added others/useless.py and updated tasks/task1.yml

---
+++ others/useless.py
@@ -0,0 +1,4 @@
+# This script has been introduced to check that non-Ansible files are ignored
+# by repominer when analyzing Ansible projects.
+
+print('Do not use me for the analysis of Ansible projects!')
